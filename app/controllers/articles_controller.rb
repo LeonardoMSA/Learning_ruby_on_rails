2d08441c12a24bab1b3ecadfcf964e3636e5c936
Leonardo Menezes Soares de Azevedo <leonardomsazevedo@gmail.com>
2024-04-02 16:24:18 -0300

    Editing articles route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,33 @@ class ArticlesController < ActionController::Base
 
     def index
         @artigos = Article.all
-        
+    end
+
+    def new
+        @artigo = Article.new()
+    end
+
+    def edit
+        @artigo = Article.find(params[:id])
+    end
+
+    def create
+        @artigo = Article.new(params.require(:article).permit(:title, :description))
+        if @artigo.save
+            puts flash[:notice] = "Artigo foi salvo com sucesso!"
+            redirect_to article_path(@artigo)
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @artigo = Article.find(params[:id])
+        if @artigo.update(params.require(:article).permit(:title, :description))
+            redirect_to article_path(@artigo)
+        else
+            render 'edit'
+        end
     end
 
 end
